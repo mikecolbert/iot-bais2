9887b8bc7982985a08217f59e1cb92edd03157da
Mike Colbert <michael-colbert@uiowa.edu>
2022-09-27 09:52:24 -0500

    added db SELECT via pymysql

--- app.py
+++ app.py
@@ -12,6 +12,14 @@ dbpass = os.environ.get('DBPASS')
 dbhost = os.environ.get('DBHOST')
 dbname = os.environ.get('DBNAME')
 
+dbconfig = {
+    'host' : dbhost, 
+    'user' : dbuser, 
+    'password' : dbpass, 
+    'db' : dbname, 
+    'client_flags': [pymysql.connector.ClientFlag.SSL],
+    'ssl_ca': './DigiCertGlobalRootG2.crt.pem'
+}
 
 @app.route('/')
 def index():
@@ -20,12 +28,43 @@ def index():
 
 @app.route('/temperatures')
 def temperatures():
-    return f"List of temperatures - {dbhost}"
+    return "List of temperatures"
    
+# GET request
+    try:
+        conn = pymysql.connector.connect(**config)
+                
+    except pymysql.connector.Error as err:
+        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+            print("Something is wrong with the user name or password")
+        elif err.errno == errorcode.ER_BAD_DB_ERROR:
+            print("Database does not exist")
+        else:
+            print(err)
+    else:
+        cur = conn.cursor()
+    #cur = conn.cursor()
+
+    query = "SELECT * FROM temperaturelog ORDER BY readTime DESC"
+    cur.execute(query)
     
+    results = cur.fetchall()
+
+    print("----------")
+    print("Total number of rows in table: ", cur.rowcount)
+    print("----------")
+    print(results)
+        
+    cur.close()
+
+    conn.close()
+            
+    return (results)
+
+
 @app.route('/sensors')
 def sensors():
-    return f"List of sensors - {dbname}"
+    return "List of sensors"
 
 
 if __name__ == '__main__':
